d20af93539f35fd06314ede10cd5164409f36160
ahall106 <105325579+ahall106@users.noreply.github.com>
2022-10-13 09:11:11 -0600

    Update env
    Mentor help on this

--- Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -6,10 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'webdevofficial', // the auth0 domain prefix
-    audience: 'image', // the audience set for the auth0 app
-    clientId: '62f56d028c6803fe0d3b04e6', // the client id generated for the auth0 app
-    callbackURL: 'https://127.0.0.1:8100', // the base url of the running ionic application. 
-    // changing this because of the following error; Error! Payload validaton error: 'Object didn't pass validation for format absoulte-uri-or-empty: https://localhost:5000/login on property initiate_login_uri(initiate login ur, just be https and cannot contain a fragment)
+    url: 'fsnd-tota.us', // the auth0 domain prefix
+    audience: 'http://localhost:5000', // the audience set for the auth0 app
+    clientId: 'wKsGwLncTHU494VVdzYBR62abrtos4Ve', // the client id generated for the auth0 app
+    callbackURL: 'http://localhost:8100', // the base url of the running ionic application.
   }
 };
